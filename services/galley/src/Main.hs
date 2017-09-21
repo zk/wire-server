3227be8c37bfc0b6da0b20ef298d3e52176726e9
Jon Elverkilde <jon@wire.com>
2017-09-21 09:24:32 +0200

    WIP: yaml options for Galley

--- services/galley/src/Main.hs
+++ services/galley/src/Main.hs
@@ -4,6 +4,15 @@ import Galley.API
 import Galley.Options
 import OpenSSL
 
-main :: IO ()
-main = withOpenSSL $ parseOptions >>= run
+import Data.Yaml (decodeFileEither, ParseException)
 
+decodeConfigFile :: FilePath -> IO (Either ParseException Opts)
+decodeConfigFile = decodeFileEither
+
+main :: IO ()
+main = withOpenSSL $ do
+  configFile <- decodeConfigFile "galley.yaml"
+  case configFile of
+    -- TODO: report config file errors
+    Left err -> print err --parseOptions >>= run
+    Right opts -> run opts
