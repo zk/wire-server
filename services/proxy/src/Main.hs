f3521fd524965bf299ab0a506bc3e29ee9e1336b
Jon Elverkilde <jon@wire.com>
2017-09-22 11:34:54 +0200

    Proxy yaml

--- services/proxy/src/Main.hs
+++ services/proxy/src/Main.hs
@@ -1,7 +1,16 @@
 module Main (main) where
 
+import Data.Yaml (decodeFileEither, ParseException)
 import Proxy.API
-import Proxy.Options (parseOptions)
+import Proxy.Options (parseOptions, Opts)
+
+decodeConfigFile :: FilePath -> IO (Either ParseException Opts)
+decodeConfigFile = decodeFileEither
 
 main :: IO ()
-main = parseOptions >>= run
+main = do
+  configFile <- decodeConfigFile "proxy.yaml"
+  case configFile of
+      -- TODO: report config file errors
+      Left err -> print err --parseOptions >>= run
+      Right opts -> run opts
